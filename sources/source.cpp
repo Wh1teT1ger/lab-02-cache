dc77d0d27355e98e6f45902e943ce5176ec22e09
Wh1teT1ger <burylov01@mail.ru>
2020-10-08 21:21:57 +0300

    fix source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -4,55 +4,55 @@
 
 void direct_passage(size_t size_kb) {
   size_t size = size_kb * 256;
-  int k = 0;
+  //int k = 0;
   int* arr = new int[size];
   for (size_t i = 0; i < size; i += 16) arr[i] = rand();
-  for (size_t i = 0; i < size; i += 16) k = arr[i];
+  for (size_t i = 0; i < size; i += 16) & arr[i];
   auto start = std::chrono::steady_clock::now();
   for (size_t j = 0; j < 1000; j++) {
-    for (size_t i = 0; i < size; i += 16) k = arr[i];
+    for (size_t i = 0; i < size; i += 16) & arr[i];
   }
   auto end = std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
-  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ns"
+  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ms"
             << std::endl;
   delete[] arr;
 }
 
 void back_passage(size_t size_kb) {
   size_t size = size_kb * 256;
-  int k = 0;
+  //int k = 0;
   int* arr = new int[size];
   for (size_t i = 0; i < size; i += 16) arr[i] = rand();
-  for (int i = size - 15; i > 0; i -= 16) k = arr[i];
+  for (int i = size - 15; i > 0; i -= 16) & arr[i];
   auto start = std::chrono::steady_clock::now();
   for (size_t j = 0; j < 1000; j++) {
     for (int i = size - 15; i > 0; i -= 16) {
-      k = arr[i];
+      &arr[i];
     }
   }
   auto end = std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
-  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ns"
+  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ms"
             << std::endl;
   delete[] arr;
 }
 
 void random_passage(size_t size_kb) {
   size_t size = size_kb * 256;
-  int k = 0;
+  //int k = 0;
   int* arr = new int[size];
   for (size_t i = 0; i < size; i += 16) arr[i] = rand();
-  for (size_t i = 0; i < size; i += 16) k = arr[i];
+  for (size_t i = 0; i < size; i += 16) & arr[i];
   auto start = std::chrono::steady_clock::now();
   for (size_t j = 0; j < 1000; j++) {
     for (size_t i = 0; i < size / 16; i++) {
-      k = arr[rand() % (size / 16) * 16];
+      & arr[rand() % (size / 16) * 16];
     }
   }
   auto end = std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
-  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ns"
+  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ms"
             << std::endl;
   delete[] arr;
 }
