87d3f936c3d260b809339a648180f9c6946649db
Wh1teT1ger <burylov01@mail.ru>
2020-10-08 20:42:07 +0300

    add files

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,22 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Burylov Denis <burylov01@mail.ru>
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
-#endif // INCLUDE_HEADER_HPP_
+#include <chrono>
+#include <iostream>
+#include <random>
+
+void direct_passage(size_t size_kb);
+
+void back_passage(size_t size_kb);
+
+void random_passage(size_t size_kb);
+
+void direct_();
+
+void back_();
+
+void random_();
+
+#endif  // INCLUDE_HEADER_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,82 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Burylov Denis <burylov01@mail.ru>
 
 #include <header.hpp>
+
+void direct_passage(size_t size_kb) {
+  size_t size = size_kb * 256;
+  int k = 0;
+  int* arr = new int[size];
+  for (size_t i = 0; i < size; i += 16) arr[i] = rand();
+  for (size_t i = 0; i < size; i += 16) k = arr[i];
+  auto start = std::chrono::steady_clock::now();
+  for (size_t j = 0; j < 1000; j++) {
+    for (size_t i = 0; i < size; i += 16) k = arr[i];
+  }
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ns"
+            << std::endl;
+  delete[] arr;
+}
+
+void back_passage(size_t size_kb) {
+  size_t size = size_kb * 256;
+  int k = 0;
+  int* arr = new int[size];
+  for (size_t i = 0; i < size; i += 16) arr[i] = rand();
+  for (int i = size - 15; i > 0; i -= 16) k = arr[i];
+  auto start = std::chrono::steady_clock::now();
+  for (size_t j = 0; j < 1000; j++) {
+    for (int i = size - 15; i > 0; i -= 16) {
+      k = arr[i];
+    }
+  }
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ns"
+            << std::endl;
+  delete[] arr;
+}
+
+void random_passage(size_t size_kb) {
+  size_t size = size_kb * 256;
+  int k = 0;
+  int* arr = new int[size];
+  for (size_t i = 0; i < size; i += 16) arr[i] = rand();
+  for (size_t i = 0; i < size; i += 16) k = arr[i];
+  auto start = std::chrono::steady_clock::now();
+  for (size_t j = 0; j < 1000; j++) {
+    for (size_t i = 0; i < size / 16; i++) {
+      k = arr[rand() % (size / 16) * 16];
+    }
+  }
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
+  std::cout << size_kb << " kb : " << elapsed_seconds.count() * 1000000 << " ns"
+            << std::endl;
+  delete[] arr;
+}
+
+void direct_() {
+  std::cout << "direct:" << std::endl;
+  for (size_t i = 64; i <= 4096; i *= 2) {
+    direct_passage(i);
+  }
+  direct_passage(6144);
+}
+
+void back_() {
+  std::cout << "back:" << std::endl;
+  for (size_t i = 64; i <= 4096; i *= 2) {
+    back_passage(i);
+  }
+  back_passage(6144);
+}
+
+void random_() {
+  std::cout << "random:" << std::endl;
+  for (size_t i = 64; i <= 4096; i *= 2) {
+    random_passage(i);
+  }
+  random_passage(6144);
+}
